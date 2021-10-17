7551263ca8b51e17aa30a196ab974e2b10926739
Matt Mattox <matt.mattox@ws3161.lrscorp.net>
2021-10-16 20:40:17 -0700

    testing index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const projButton = document.getElementById('proj-button')
 
+projButton.addEventListener('click', alertMe)
+
+function alertMe() {
+    console.log("Clicked!!!")
+}
+
+
+function test() {
+    console.log('Does this work?')
+}
+
+test();
